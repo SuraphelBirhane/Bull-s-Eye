3321175d9eda0a7e92b438d411e98973db05844d
SuraphelBrihane <suraphel@kemercode.com>
2022-02-19 20:06:47 +0300

    initial commit

---
+++ Bull'sEye(My Version)/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  Bull'sEye(My Version)
+//
+//  Created by Suraphel on 2/17/22.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dissmissScreen(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
